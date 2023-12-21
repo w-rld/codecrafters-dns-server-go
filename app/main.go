5f33b7ce91a2b6ed149472fcdd1f918961bfe895
w-rld <worldcreator@hotmail.com>
2023-12-21 21:41:34 +0100

    pass 2nd stage

--- app/main.go
+++ app/main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	// Uncomment this block to pass the first stage
 	"net"
 )
+
+type DNSMessage struct {
+	Header DNSResponseHeader
+	content string
+}
+
+type DNSHeaderFlags struct {
+	QR bool // Query Response
+	OPCODE uint8 // Operation Code
+	AA bool // Authoritative Answer
+	TC bool // Truncate Message
+	RD bool // Recursive Desired
+	RA bool // Recursion Available
+	Z uint8 // Reserved
+	RCODE uint8 // Response Code
+}
+
+type DNSResponseHeader struct {
+	ID uint16 // Packet Identifier
+	Flags DNSHeaderFlags
+	QDCOUNT uint16 // Question Count
+	ANCOUNT uint16 // Answer Count
+	NSCOUNT uint16 // Authority Count
+	ARCOUNT uint16 // Additional Count
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -36,12 +62,67 @@ func main() {
 		receivedData := string(buf[:size])
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 
-		// Create an empty response
-		response := []byte{}
+		// Create a response
+		header := CreateResponse();
+		response := Serialize(header)
+		fmt.Printf("Serialized response header bytes %b", response)
 
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
+	}
+}
+
+func Serialize(header DNSResponseHeader) []byte {
+	result := make([]byte, binary.Size(header))
+	binary.BigEndian.PutUint16(result[:2], header.ID)
+
+	var flags uint16
+	if header.Flags.QR {
+		flags |= 1 << 15
+	}
+	flags |= uint16(header.Flags.OPCODE) << 11
+	if header.Flags.AA {
+		flags |= 1 << 10
+	}
+	if header.Flags.TC {
+		flags |= 1 << 9
+	}
+	if header.Flags.RD {
+		flags |= 1 << 8
+	}
+	if header.Flags.RA {
+		flags |= 1 << 7
+	}
+	flags |= uint16(header.Flags.Z) << 4
+	flags |= uint16(header.Flags.RCODE)
+	binary.BigEndian.PutUint16(result[2:4], flags)
+
+	binary.BigEndian.PutUint16(result[4:6], header.QDCOUNT)
+	binary.BigEndian.PutUint16(result[6:8], header.ANCOUNT)
+	binary.BigEndian.PutUint16(result[8:10], header.NSCOUNT)
+	binary.BigEndian.PutUint16(result[10:12], header.ARCOUNT)
+
+	return result
+}
+
+func CreateResponse() DNSResponseHeader {
+	return DNSResponseHeader{
+		ID: 1234,
+		Flags: DNSHeaderFlags {
+			QR: true,
+			OPCODE: 0x0,
+			AA: false,
+			TC: false,
+			RD: false,
+			RA: false,
+			Z: 0x0,
+			RCODE: 0x0,
+		},
+		QDCOUNT: 0x0,
+		ANCOUNT: 0x0,
+		NSCOUNT: 0x0,
+		ARCOUNT: 0x0,
 	}
 }
